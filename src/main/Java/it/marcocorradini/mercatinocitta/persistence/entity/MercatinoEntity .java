30972955c1f9c66b690edadad5fa0427bad54804
Marco Corradini <marco.corradini@itsincom.it>
2026-05-19 12:07:55 +0200

    Created entities: - Tema - MercatinoEntity

---
+++ src/main/Java/it/marcocorradini/mercatinocitta/persistence/entity/MercatinoEntity .java
@@ -0,0 +1,68 @@
+package it.marcocorradini.mercatinocitta.persistence.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="mercatini")
+public class MercatinoEntity  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titolo;
+    private String descrizione;
+    @Column(nullable = false)
+    private LocalDateTime dataOra;
+    @Column(nullable = false)
+    private String piazza;
+    @Enumerated(EnumType.STRING)
+    private Tema tema;
+    @Column(nullable = false)
+    private Integer banchiTotali;
+    @Column(nullable = false)
+    private Integer banchiAssegnati=0;
+
+    public MercatinoEntity(){}
+    public Long getId(){
+        return id;
+    }
+    public String getTitolo(){
+        return titolo;
+    } public void setTitolo(String titolo){
+        this.titolo=titolo;
+    }
+    public LocalDateTime getDataOra(){
+        return dataOra;
+    } public void setDataOra(LocalDateTime dataOra){
+        this.dataOra=dataOra;
+    }
+    public String getPiazza(){
+        return piazza;
+    } public void setPiazza(String piazza){
+        this.piazza=piazza;
+    }
+    public Tema getTema(){
+        return tema;
+    } public void setTema(Tema tema){
+        this.tema=tema;
+    }
+    public Integer getBanchiTotali(){
+        return banchiTotali;
+    } public void setBanchiTotali(Integer banchiTotali){
+        this.banchiTotali=banchiTotali;
+    }
+    public Integer getBanchiAssegnati(){
+        return banchiTotali;
+    } public void setBanchiAssegnati(Integer banchiAssegnati){
+        this.banchiAssegnati=banchiAssegnati;
+    }
+}
---
+++ src/main/Java/it/marcocorradini/mercatinocitta/persistence/entity/Tema.java
@@ -0,0 +1,8 @@
+package it.marcocorradini.mercatinocitta.persistence.entity;
+
+public enum Tema {
+    LIBRI,
+    ABBIGLIAMENTO,
+    VINTAGE,
+    SVUOTACANTINE
+}
