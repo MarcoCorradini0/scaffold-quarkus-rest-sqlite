0eae22ba61308602cbbeb5a89766da4453caaaf0
Marco Corradini <marco.corradini@itsincom.it>
2026-05-20 11:17:56 +0200

    imports

--- src/main/Java/it/marcocorradini/mercatinocitta/web/resource/MercatinoResource.java
+++ src/main/Java/it/marcocorradini/mercatinocitta/web/resource/MercatinoResource.java
@@ -3,15 +3,11 @@ package it.marcocorradini.mercatinocitta.web.resource;
 import java.util.List;
 
 import it.marcocorradini.mercatinocitta.persistence.entity.MercatinoEntity;
+import it.marcocorradini.mercatinocitta.persistence.entity.Tema;
 import it.marcocorradini.mercatinocitta.persistence.repository.MercatinoRepository;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -58,7 +54,6 @@ public class MercatinoResource {
         } else {
             return Response.ok(m).build();
         }
-        return Response.ok(m).build();
     }
 
     @POST // Create
